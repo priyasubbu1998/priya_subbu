d07949db447a5f2d6aa2fcae300c11fad12f4632
priyasubbu1998 <112119326+priyasubbu1998@users.noreply.github.com>
2022-08-31 14:49:48 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
                 id: 3,
                 title: "linkedin",
                 path: "https://Whatsapp.com",
-                icon: "WhatsApp",
+                icon: "logo.png",
             },
         ],
         contact: {
